1a8dc3fea19ee0ed92846420282c85bad0f82b49
jochen <18362961791@163.com>
2017-03-21 14:25:02 +0800

    angular routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute,} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  query : string;
+
+  constructor(private route : ActivatedRoute){
+    console.log(111);
+    /*this.route
+      .queryParams
+      .subscribe(params => {this.query = params['query'] || '';console.log(this.query)})*/
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ArtistComponent } from './artist/artist.component';
+import { AlbumComponent } from './album/album.component';
+import { TrackComponent } from './track/track.component';
+
+import {SPOTIFY_PROVIDERS} from './spotify.service';
+
+import {routes} from "./routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ArtistComponent,
+    AlbumComponent,
+    TrackComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    SPOTIFY_PROVIDERS
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
